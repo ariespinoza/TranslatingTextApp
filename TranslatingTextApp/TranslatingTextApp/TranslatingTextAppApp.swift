32ad858e7cbf565f9ae58508de2e6d370fa6c1d5
ariespinoza <a01645270@tec.mx>
2025-10-06 12:29:41 -0600

    Inicio

---
+++ TranslatingTextApp/TranslatingTextApp/TranslatingTextAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatingTextAppApp.swift
+//  TranslatingTextApp
+//
+//  Created by Alumno on 06/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TranslatingTextAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
